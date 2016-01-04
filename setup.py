084ccd2622e2e7c47bb8b0231bc6d22f8777cf5d
Paul Healy <lmiphay@gmail.com>
2016-01-04 22:15:58 +0000

    call make install from setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,21 @@
 from distutils.core import setup
+from distutils.command.install_data import install_data
+import subprocess
+
+class oam_install(install_data):
+    def run(self):
+        install_data.run(self)
+        print("Running")
+        # , cwd=os.path.join(dir, 'packagename')
+        subprocess.call(['make', 'install'])
+        print("Complete")
 
 setup(name='oam',
-      version='1.0',
+      version='5.0',
       description='gentoo-oam implementation',
       author='Paul Healy',
       url='https://github.com/lmiphay/gentoo-oam',
       packages=['oam'],
-      scripts=['bin/oam']
+      scripts=['bin/oam'],
+      cmdclass={"install_data": oam_install},
       )
