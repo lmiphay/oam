2a494b585351ef3f5e34630d59021129e8f4a5d0
Paul Healy <lmiphay@gmail.com>
2016-01-10 18:31:34 +0000

    lose some debug

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ import os
 class oam_install(install_data):
     def run(self):
         install_data.run(self)
-        print("Running: " + str(os.environ))
         subprocess.call(['make', 'DESTDIR=' + os.environ['ED'], 'install'])
-        print("Complete")
 
 setup(name='oam',
       version='5.0',
