fd7afc9e433d97093b574ad72675c73c32fac482
Paul Healy <lmiphay@gmail.com>
2016-01-16 12:45:01 +0000

    more filtering

--- oam/pretend.py
+++ oam/pretend.py
@@ -11,7 +11,6 @@ class Pretend:
 
     SKIPLINES = {
         'These are the packages that would be merged, in order:',
-        'Calculating dependencies  .... .... done!'
         'Total: 0 packages, Size of downloads: 0 KiB',
         ' * Use eselect news read to view new items.',
         ''
@@ -20,6 +19,9 @@ class Pretend:
     def __init__(self, target = 'world'):
         self.target = target
         self.logger = logging.getLogger("oam.pretend")
+
+    def filter(self, line):
+        return line in self.SKIPLINES or lines.startswith('Calculating dependencies')
         
     def run(self):
         self.logger.log(logging.DEBUG, 'pretend cmd: %s', str(self.CMD))
@@ -27,7 +29,7 @@ class Pretend:
         try:
             for line in subprocess.check_output(self.CMD + [self.target],
                                                 stderr=subprocess.STDOUT).splitlines():
-                if line not in self.SKIPLINES:
+                if not self.filter(line):
                     print(line)
         except subprocess.CalledProcessError as e:
             self.logger.log(logging.ERROR, 'pretend failed for: %s', self.target)
