b1fcbb01738976ea662229a625aa92638e843daf
Paul Healy <lmiphay@gmail.com>
2016-02-20 22:36:35 +0000

    add some initial fabric taks

---
+++ oam/fabric/bootstrap.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import fabric.contrib.files
+from fabric.api import *
+
+env.hosts = [ '192.168.1.100' ]
+
+def if_missing_do(sentinel, cmd):
+    if not fabric.contrib.files.exists(sentinel):
+        run(cmd)
+
+def is_dir(path):
+    with settings(warn_only=True):
+        return run('test -d ' + path).success
+
+def is_file(path):
+    with settings(warn_only=True):
+        return run('test -f ' + path).success
+
+def setup_layman(overlay):
+    if_missing_do('/usr/bin/layman', 'emerge app-portage/layman')
+    if_missing_do('/etc/portage/repos.conf', 'mkdir -p /etc/portage/repos.conf && layman-updater -R')
+    if_missing_do('/var/lib/layman' + overlay, 'layman -a ' + overlay)
+    run('layman -s ' + overlay)
+
+def install_keywords():
+    if not fabric.contrib.files.is_link('/etc/portage/package.keywords/gentoo-oam.keywords'):
+        if is_file('/etc/portage/package.keywords'):
+            run('mv /etc/portage/package.keywords /etc/portage/pre-oam-package.keywords')
+        run('mkdir -p /etc/portage/package.keywords')
+        run('ln -s /var/lib/layman/lmiphay/gentoo-oam.keywords /etc/portage/package.keywords/'
+        if is_file('/etc/portage/pre-oam-package.keywords'):
+            run('mv /etc/portage/pre-oam-package.keywords /etc/portage/package.keywords/')
+@task
+def bootstrap():
+    setup_layman('lmiphay')
+    install_keywords()
+    run('emerge gentoo-oam')
+    run('oam-go')
+
+def installed(pkg):
+    run('qlist --exact--quiet --installed ' + pkg)
+    #app-portage/portage-utils:qfile
+
---
+++ oam/fabric/lxc.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+
+import logging
+from fabric.api import *
+
+class Create(Task):
+
+    name        = 'create' # fabric task name
+
+    # generic lxc-create
+    CMD         = 'lxc-create --template=gentoo'
+    CONFIG_PATH = '--lxcpath=/etc/lxc' # -P/--lxcpath, value=`lxc-config lxc.lxcpath`, default='/var/lib/lxc'
+    ROOTFS_PATH = '--dir='             # --dir=DIR Place rootfs directory under DIR
+    LOG         = '--logfile=/var/log/oam/lxc.log --logpriority=DEBUG'
+
+    # gentoo specific
+    STAGE3      = '--tarball=' # /lxc/stage3-amd64-20160218.tar.bz2 '
+    PORTAGE     = '--portage-dir=/usr/portage'
+    # ROOTFS_PATH = '--rootfs=/lxc' # duplicate generic --dir option...
+    # FLUSH_CACHE = '--flush-cache'
+
+    def __init__(self, name, tarball):
+        self.name = name
+        self.cmd = self.CMD + '--name=' + name + ' ' + self.CONFIG_PATH
+        self.cmd = self.cmd + ' ' + self.LOG
+        
+        self.cmd = self.cmd + ' -- '
+        
+        self.cmd = self.cmd + self.PORTAGE
+        self.cmd = self.cmd + self.STAGE3 + ' ' + tarball
+
+    def dummy_portage_snapshot(self):
+        """
+        never download the portage snapshot
+        """
+        run('mkdir -p /var/cache/lxc/gentoo') 
+        run('mkdir touch /var/cache/lxc/gentoo/portage.tbz')
+        
+    def run(self, environment):
+        self.dummy_portage_snapshot()
+        run(self.cmd)
+        
+# The default configuration used for all containers at creation time is taken from
+# /etc/lxc/default.conf
+# => rootfs of container is : /lxc/oam2/rootfs
+# => config of container is : /lxc/oam2/config
+# -f, --config=file  Initial configuration file
+# -p|--path)             path=$2; shift 2;;
+# run('ln -s /lxc/oam2/config .')
+
+class Start(Task):
+    
+    name        = 'start' # fabric task name
+    
+    CMD         = 'lxc-start --name='
+    CONSOLE_LOG = '--console-log=/var/log/oam/console.log.'
+    # FOREGROUND  = '--foreground'
+    
+    def __init__(self, name):
+        self.cmd = self.CMD + name + ' ' + self.CONSOLE_LOG + name
+
+    def run(self):
+        run(self.cmd)
+        
+class Stop(Task):
+    
+    name        = 'stop' # fabric task name
+    
+    CMD         = 'lxc-stop --name='
+    
+class Destroy(Task):
+
+    name        = 'destroy' # fabric task name
+    
+    CMD         = 'lxc-destory --force --name='
+
