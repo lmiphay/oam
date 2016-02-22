522652452323ebfd8b5e7ab5ab4b665044711e92
Paul Healy <lmiphay@gmail.com>
2016-02-22 22:04:58 +0000

    remove Makefile call hack

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from distutils.core import setup
 from distutils.command.install_data import install_data
-import subprocess
-import os
-
-class oam_install(install_data):
-    def run(self):
-        install_data.run(self)
-        subprocess.call(['make', 'DESTDIR=' + os.environ['ED'], 'install'])
 
 setup(name='oam',
       version='5.0',
@@ -15,7 +8,6 @@ setup(name='oam',
       url='https://github.com/lmiphay/gentoo-oam',
       packages=['oam'],
       scripts=['bin/oam'],
-      cmdclass={"install_data": oam_install},
       data_files=[('share/gentoo-oam',
                    ['share/gentoo-oam-functions.sh',
                     'share/gentoo-oam-multitail.conf',
