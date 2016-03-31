70be418c740a9a5a760640a398fb863b34a48a86
Paul Healy <lmiphay@gmail.com>
2016-03-31 20:46:23 +0100

    chdir to the run directory

--- oam/watch.py
+++ oam/watch.py
@@ -10,46 +10,50 @@ from .cmd import cli
 import glob
 import time
 import datetime
-from version import __version__
+from version import get_version
+
+ROW1_HEIGHT = '9'
+ROW2_HEIGHT = '9'
+
+COL1_WIDTH = '45'
+COL2_WIDTH = '40'
+
+def last_date():
+    datedir = sorted(glob.glob(os.getenv('OAM_LOGDIR', '/var/log/oam') + '/2*'))
+    if len(datedir) > 0:
+        return datedir[-1]
+    else:
+        return None
 
 """
 """
 class Watch(object):
 
-    ROW1_HEIGHT = '9'
-    ROW2_HEIGHT = '9'
-
-    COL1_WIDTH = '45'
-    COL2_WIDTH = '40'
-
     HOSTNAME = os.uname()[1]
     KERNEL_VER = os.uname()[2]
 
     def __init__(self, row1, row2, col1, col2):
+        self.logger = logging.getLogger("oam.watch")
         self.row1 = row1
         self.row2 = row2
         self.col1 = col1
         self.col2 = col2
-        self.watch_dir = self.lastdate()
+        self.watch_dir = last_date()
 
     def title(self):
-        return self.HOSTNAME + '/oam/' + self.watch_dir + '/' + \
-            self.KERNEL_VER + ' gentoo-oam V' + __version__
-
-    def lastdate(self):
-        datedir = glob.glob(os.getenv('OAM_LOGDIR', '/var/log/oam') + '/2*')
-        if len(datedir)>1:
-            return datedir[-1]
-        else:
-            return datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d')
+        return "{}/oam/{}/{} gentoo-oam V{}".format(self.HOSTNAME, os.path.basename(self.watch_dir), self.KERNEL_VER, get_version())
 
     def make_conf(self):
         if os.path.exists('/etc/make.conf'):
             return '/etc/make.conf'
         elif os.path.exists('/etc/portage/make.conf'):
             return '/etc/portage/make.conf'
+        else:
+            return ""
 
     def run(self):
+        os.chdir(self.watch_dir) # this makes the multitail command line a _lot_ shorter
+
         cmd = [ 'multitail',
                 '-x', self.title(),
                 '--config', '/usr/share/gentoo-oam/gentoo-oam-multitail.conf',
@@ -59,36 +63,44 @@ class Watch(object):
 
                 '-wh', self.row1, '/usr/share/gentoo-oam/oam-watch.help', # win1: help
                 '--retry',
-                '-wh', self.row2, self.watch_dir + '/oam.log',            # win2: oam activity log
+                '-wh', self.row2, 'oam.log',                              # win2: oam activity log
                 '-j',                                                     # win3: stdin - genlop piped
 
                 '-wh', self.row1, '-l', 'oam emergelog',                  # win4: /var/log/emerge.log
                 '--retry-all',
-                '-wh', self.row2, self.watch_dir + '/error.log',          # win5: general error log
+                '-wh', self.row2, 'error.log',                            # win5: general error log
 
                 '--mark-change', '-T',
 
-                '-i', self.watch_dir + '/sync.log',                       # win6: misc logs
-                '-I', self.watch_dir + '/merge.log',
-                '--label', 'blocks ', '-I', self.watch_dir + '/blocks.log',
-                '-t', 'sync/merge/blocks/kernel ',
-                '-I', self.watch_dir + '/kernel.log',
-                '-I', self.watch_dir + '/summary.log'# ,
-                # os.getenv('OAM_MULTITAIL_EXTRA_OPT', '')
+                '-i', 'sync.log',                                         # win6: misc logs
+                '-I', 'merge.log',
+                '--label', 'blocks ', '-I', 'blocks.log',
+                '-t', 'sync/merge/blocks/kernel ',                        # set title in statusline
+                '-I', 'kernel.log',
+                '-I', 'summary.log'
         ]
-        #subprocess.call(cmd)
+
+        extra_opt = os.getenv('OAM_MULTITAIL_EXTRA_OPT', None)
+        if extra_opt:
+            cmd.append(extra_opt)
+
         genlop = subprocess.Popen(['oam', 'genlop'], stdout=subprocess.PIPE)
-        multitail = subprocess.Popen(cmd, stdin=genlop.stdout)
+        multitail = subprocess.Popen(cmd, stdin=genlop.stdout, env=dict(os.environ, OAM_MAKECONF=self.make_conf()))
+
         return multitail.wait()
 
 @cli.command()
-@click.option('--row1', default=Watch.ROW1_HEIGHT, envvar='OAM_ROW1_HEIGHT')
-@click.option('--row2', default=Watch.ROW2_HEIGHT, envvar='OAM_ROW2_HEIGHT')
-@click.option('--col1', default=Watch.COL1_WIDTH, envvar='OAM_ROW1_HEIGHT')
-@click.option('--col2', default=Watch.COL2_WIDTH, envvar='OAM_ROW2_HEIGHT')
+@click.option('--row1', default=ROW1_HEIGHT, envvar='OAM_ROW1_HEIGHT')
+@click.option('--row2', default=ROW2_HEIGHT, envvar='OAM_ROW2_HEIGHT')
+@click.option('--col1', default=COL1_WIDTH, envvar='OAM_ROW1_HEIGHT')
+@click.option('--col2', default=COL2_WIDTH, envvar='OAM_ROW2_HEIGHT')
 def watch(row1, row2, col1, col2):
     """Dashboard overview of current system update"""
-    return Watch(row1, row2, col1, col2).run()
+    logdir = last_date()
+    if logdir:
+        return Watch(row1, row2, col1, col2).run()
+    else:
+        sys.exit("No log directory found")
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG,
