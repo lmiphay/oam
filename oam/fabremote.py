2c93e75bab594542454d0c862128dc84de935b5d
Paul Healy <lmiphay@gmail.com>
2016-04-09 20:32:29 +0100

    work in progress

---
+++ oam/fabremote.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import sys
+import os
+import logging
+import click
+import subprocess
+import re
+import datetime
+from .cmd import cli
+from .serverslog import ServersLog
+import fabric
+
+def is_lxc():
+    """Return True if the server is an lxc, False otherwise"""
+    return fabric.contrib.files.contains('/proc/1/cgroup', '/lxc/')
+
+def is_docker():
+    """Return True if the server is a docker, False otherwise"""
+    return fabric.contrib.files.contains('/proc/1/cgroup', '/docker/')
+
+@fabric.api.task
+def fab_remote(cmd, logger):
+    with fabric.api.settings(warn_only=True):
+        return fabric.api.run(cmd,
+                              stdout=logger.out(env.host),
+                              stderr=logger.err(env.host),
+                              combine_stderr=False)
+
+class FabRemote(object):
+
+    def __init__(self, servers):
+        self.servers = servers
+        self.logger = ServersLog(servers)
+
+    def drive(self, step):
+        result = execute(fab_remote,
+                         step['cmd'],
+                         self.logger.step(step['log']),
+                         hosts=self.servers)
+        if result.failed:
+            logger.fail(result)
+        return result
+
+@cli.command()
+@click.option('--log', default='fabremote', help='')
+@click.argument('servers')
+@click.argument('cmd')
+def fabremote(log, servers, cmd):
+    """"""
+    step = {
+        'cmd': cmd,
+        'log': log
+        }
+    return Fabremote(servers).drive(step)
---
+++ oam/steps.py
@@ -0,0 +1,83 @@
+
+import logging
+
+OAM_VERBOSE="--verbose --verbose-conflicts"
+OAM_EMERGE_OPTS="--backtrack=50 --deep "
+
+steps = {
+    'sync': {
+        'cmd': 'emaint --auto sync',
+        'log': 'sync',               # portage step checks this log to see if portage should be merged first
+        'exclude': ['lxcs']
+        },
+    'layman':
+        'cmd': 'layman --sync=ALL --nocolor',
+        'log': 'layman'
+        },
+    'eix': {
+        'cmd': 'eix-update --nocolor',
+        'log': 'eix'
+        },
+    'glsa': {
+        'cmd': 'glsa-check --test --verbose --nocolor all',
+        'log': 'security',
+        'level': logging.WARNING
+        },
+    'fetch': {
+        'cmd': 'emerge --fetchonly {} --update world'.format(OAM_EMERGE_OPTS),
+        'log': 'fetch',
+        'exclude': ['lxcs']
+        },
+    'portage': {
+        'cmd': 'emerge --update {} sys-apps/portage'.format(OAM_VERBOSE),
+        'log': 'merge'
+        },
+    'update': {
+        'cmd': 'emerge {} {} --update world'.format(OAM_EMERGE_OPTS. OAM_VERBOSE),
+        'log': 'merge'
+        },
+    'revdep': {
+        'cmd': 'revdep-rebuild --nocolor --ignore',
+        'log': 'revdep'
+        },
+    'python': {
+        'cmd': 'python-updater -eall',
+        'log': 'python'
+        },
+    'perl': {
+        'cmd': 'perl-cleaner --all',
+        'log': 'perl'
+        },
+    'preserved': {
+        'cmd': 'emerge --keep-going {} @preserved-rebuild'.format(OAM_VERBOSE),
+        'log': 'merge'
+        },
+    'eclean-distfiles': {
+        'cmd': 'eclean --nocolor distfiles',
+        'log': 'clean',
+        'exclude': ['lxcs']
+        },
+    'eclean-kernel': {
+        'cmd': 'eclean-kernel --num=3 --exclude=config --no-mount',
+        'log': 'clean',
+        'exclude': ['lxcs']
+        },
+    'qcheck': {
+        'cmd': "qcheck --all --nomtime --nocolor | egrep 'AKF|MD5-DIGEST' | sort",
+        'log': 'qcheck'
+        },
+    }
+
+flows = {
+    'sync'  : ['sync', 'layman', 'eix', 'glsa', 'fetch' ],
+    'update': ['portage', 'update', 'revdep', 'python', 'perl', 'preserved'],
+    'clean' : ['eclean-distfiles', 'eclean-kernel'. 'qcheck']
+    }
+
+flows['weekly'] = flows['sync'] + flows['update'] + flows['clean']
+
+flows['officenet'] = [
+    [flows['sync']],
+    [flows['update'], {'server1': flows['weekly'] } ]
+    [flows['clean']],
+]
