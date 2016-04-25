dab15f18cf7cdd9afb48ceb86113314ee753aea8
Paul Healy <lmiphay@gmail.com>
2016-04-25 22:15:23 +0100

    rework dir handling

--- oam/daylog.py
+++ oam/daylog.py
@@ -13,40 +13,45 @@ import glob
 import re
 from .cmd import cli
 
+LOG_DIR = os.getenv('OAM_LOGDIR', '/var/log/oam')
+
 def today():
     return datetime.datetime.fromtimestamp(time.time()).strftime('%Y%m%d')
+
+def day_runs():
+    return sorted(filter(re.compile(r'\d{8}').match, os.listdir(LOG_DIR)))
+
+def last_day():
+    datedir = day_runs()
+    if len(datedir) > 0:
+        return datedir[-1]
+    else:
+        return None
 
 class DayLog(object):
 
     def __init__(self, day=today()):
-        self.log_dir = os.getenv('OAM_LOGDIR', '/var/log/oam')
-        self.starting_day = day
+        self.day = day
+        self.day_dir = "{}/{}".format(LOG_DIR, day)
 
-    def log_dir(self):
-        return log_dir
+    def day_dir(self):
+        return self.day_dir
 
     def current_day(self):
-        return self.starting_day
-
-    def last_day(self):
-        datedir = self.day_runs()
-        if len(datedir) > 0:
-            return datedir[-1]
-        else:
-            return None
-
-    def day_runs(self):
-        return sorted(filter(re.compile(r'\d{8}').match, os.listdir(self.log_dir)))
+        return self.day
 
     def timed_runs(self):
-        return sorted(filter(re.compile(r'\d{6}').match, os.listdir("{}/{}".format(self.log_dir, self.current_day()))))
+        if os.path.isdir(self.day_dir):
+            return sorted(filter(re.compile(r'\d{6}').match, os.listdir(self.day_dir)))
+        else:
+            return []
 
 DAYLOG = DayLog()
 
 @cli.command()
 def logdir():
     """Return the configured oam log directory"""
-    print(DAYLOG.log_dir())
+    print(LOG_DIR)
     return 0
 
 @cli.command()
@@ -61,17 +66,23 @@ def lastday():
     Return the last day we appear to have log files for; eg.
     /var/log/oam/20160403
     """
-    print(DAYLOG.last_day())
+    print(last_day())
     return 0
 
 @cli.command()
 def dayruns():
     """Return the list of runs"""
-    print(DAYLOG.day_runs())
+    print(day_runs())
     return 0
 
 @cli.command()
 def timedruns():
     """Return the list of runs for the current day"""
     print(DAYLOG.timed_runs())
+    return 0
+
+@cli.command()
+def lasttimedruns():
+    """Return the list of runs for the last day"""
+    print(DayLog(last_day()).timed_runs())
     return 0
