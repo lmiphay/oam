b16ac37d7ac9ae480f2b6640c69d3c380787b8b9
Paul Healy <lmiphay@gmail.com>
2016-07-24 20:06:32 +0100

    iterate over a list of facts

--- oam/facts.py
+++ oam/facts.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+import pprint
 import click
+import importlib
 from .cmd import cli
 
-@cli.group()
+FACT = ['oam.fact.profile', 'oam.fact.server', 'oam.fact.synchistory']
+FACTMOD = []
+
+def import_facts():
+    global FACTMOD
+    FACTMOD = map(importlib.import_module, FACT)
+
+def get_facts():
+    if len(FACTMOD) == 0:
+        import_facts()
+
+    result = {}
+    for mod in FACTMOD:
+        result.update(mod.fact())
+
+    return result
+
+@cli.group(invoke_without_command=True)
 def facts():
     """Server/build information"""
-    pass
+    print(pprint.pformat(get_facts()))
