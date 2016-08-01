5a4984253b2d0e8835b1e1b041062ba00aad49af
Paul Healy <lmiphay@gmail.com>
2016-08-01 21:07:54 +0100

    add logdate fact

---
+++ oam/fact/logdate.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+import os
+import click
+from oam.facts import facts
+from oam.daylog import last_day, DayLog
+
+def fact(day = last_day()):
+    """Return a string represent the day a report is being run for"""
+    return { 'logdate': os.path.basename(day) }
+
+@facts.command()
+def logdate():
+    """A list of runs made for the day"""
+    print(fact()['logdate'])
+    return 0
