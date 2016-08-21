393dba11d00eb103738157e59090dd2e276e5d1d
Paul Healy <lmiphay@gmail.com>
2016-08-21 19:52:58 +0100

    add detach call

--- oam/dsl/flow.py
+++ oam/dsl/flow.py
@@ -6,12 +6,16 @@ class Flow(object):
         self.description = description
         self.hosts = Hosts()
         self.steps = Steps()
+        self.run_detached = False
 
     def on(self, hosts):
         self.hosts.add(hosts)
         return self
 
-    def add(self, steps):
+    def perform(self, steps):
         self.steps.add(steps)
         return self
+
+    def detach(self, run_detached=True):
+        self.run_detached = run_detached
 
