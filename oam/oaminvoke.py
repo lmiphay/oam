aedfe729dd4d4cd05da7c568cd7f28b97c196853
Paul Healy <lmiphay@gmail.com>
2017-02-14 22:11:45 +0000

    demo invoke tasks

--- oam/oaminvoke.py
+++ oam/oaminvoke.py
@@ -1,78 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import os
 import logging
+import click
+from .cmd import cli
 
-# todo:
-# 1. setup logging for subprocesses (file destinations and default level) - proxy for runner
-# 2. for a particular target select an appropriate backend (e.g. invoke.Local or pyssh or pyexpect). -> inject a runner
+import invoke
 
-#  prog = Program(config_class=x)
-#  runner_class = self.config.get('runner', Local)
-
-# 3. point at a particular tasks file? or a set of files/dirs -> config
-# 4. parameterise some commands - pull from oam module (oam.param.x)
-# 5. flags: not in lxc, must-not-fail, optional-but-warn, quiet-on-fail
-# 6. parallel task execution
-
-# 7. want to log the actual command
-# 8. need to ansi filter some output
-# 9. select line buffering
-# 10. stop stdout buffering
-# 11. pretend mode
-
-def is_executable(filename):
-    """Return True if file exists and is executable"""
-    return os.path.isfile(filename) and os.access(filename, os.X_OK)
-
-def check_for_executable(filename):
-    """Check if file is an executable and log a warning if it is missing"""
-    if not is_executable(filename):
-        logging.warning('{} not found'.format(filename))
-        return False
-    return True
-
-def opts(ident='merge', mergelog=True):
-    return {
-        'out_stream': get_logstream(ident),
-        'err_stream': get_errstream('blocks'),
-        'hide': True
-        }
-
-def is_update_available(atom):
-    pass
-
-def preserved_libs():
-    pass
-
-def oamrun(ctx, cmd, options):
-    logger.info('running %s', cmd)
-    if not ctx.pretend:
-        ctx.run(cmd, options)
-
-# OAM_CONFIG = {
-#     'emerge_opts': ['OAM_EMERGE_OPTS', '--keep-going --backtrack=50 --deep --verbose --verbose-conflicts'],
-#     }
-
-# def config(item):
+@invoke.task
+def sync(ctx):
+    ctx.run('echo sync!')
     
-def oam_config(a):
-    return ""
-
-# # push/pop
-# @task
-# def push(ctx, logfile='merge', loglevel=logging.INFO):
-#     # ctx.config.runner.logging(logfile, loglevel)
-#     # out_stream=
-#     #def run(ctx, cmd):
-#     #    # watchers=[]
-#     #    ctx.run(command=cmd, hide=True, out_stream=x.stdout, err_stream=x.stderr)
-#     pass
-
-# class OamLocal(invoke.runners.Local):
+@invoke.task
+def summary(ctx):
+    ctx.run('echo summary!')
     
-#     def __init__(self, context):
-#         super(OamLocal, self).__init__(context)
+@invoke.task(default=True, pre=[sync], post=[summary])
+def update(ctx):
+    ctx.run('echo update!')
+
+@cli.command(name='invoke')
+@click.argument('tasks', nargs=-1)
+def oaminvoke(tasks):
+    """Sequentially invoke one or more tasks"""
+    logger = logging.getLogger("oam.invoke")
+    
+    ns = invoke.Collection()
+    ns.add_task(update)
+    ns.add_task(sync)
+    ns.add_task(summary)
+    
+    executor = invoke.Executor(ns)
+    
+    for task in tasks:
+        logger.log(logging.INFO, 'invoking %s', task)
+        result = executor.execute((task, {}))
+        logger.log(logging.INFO, '  result %s', str(result))
         
-#     def start(self, command, shell, env):
-#        pass
+    return 0
