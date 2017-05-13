446377b0f5dcf72491cdd22cb4b6e81297e99b17
Paul Healy <lmiphay@gmail.com>
2017-05-13 22:16:17 +0100

    add multicolumn report

--- oam/review.py
+++ oam/review.py
@@ -4,31 +4,65 @@ from __future__ import print_function
 
 import os
 import subprocess
+import pprint
+
 import click
 from .cmd import cli
 from oam.daylog import last_day
 from oam.status import Status
 
+STATUS_CMD = 'oam status --yaml-format --day='
+
 class Review(object):
 
-    def __init__(self, day, servers):
+    def __init__(self, day, multicolumn, width, servers):
         self.day = day
+        self.multicolumn = multicolumn
+        self.width = width
         if len(servers) == 0:
             self.servers = os.getenv('OAM_REVIEW_HOSTS', 'localhost').split()
         else:
             self.servers = servers
 
     def run(self):
+        cmd = '{}{}'.format(STATUS_CMD, self.day)
+        self.report = {}
         for host in self.servers:
+            self.report[host] = [ 'server: {}'.format(host) ]
             if host == 'localhost':
-                print(Status(day).run())
+                self.report[host].extend(Status(self.day, yaml_format=True).run().split('\n'))
             else:
-                subprocess.call('ssh {} oam status --day={}'.format(host, self.day), shell=True)
+                self.report[host].extend(subprocess.check_output('ssh {} {}'.format(host, cmd), shell=True).split('\n'))
+        if self.multicolumn:
+            return self.render_multicolumn()
+        else:
+            return [self.report[host] for host in self.servers]
+
+    def render_multicolumn(self):
+        i = 0
+        result = []
+        while True:
+            multicol = ''
+            finished = True
+            for host in self.servers:
+                if i<len(self.report[host]):
+                    line = self.report[host][i].ljust(self.width, '.')[0:self.width]
+                    finished = False
+                else:
+                    line = ','*self.width
+                multicol = multicol + line + ' |'
+            result.append(multicol)
+            i += 1
+            if finished:
+                break
+        return result
 
 @cli.command()
 @click.option('--day', default=last_day(), help='day logs to process')
-@click.argument('servers', nargs=-1)
-def review(day, servers):
+@click.option('--multicolumn', is_flag=True, help='write a multicolumn hosts report')
+@click.option('--width', default=78, help='column width in multicolumn mode')
+@click.argument('servers', nargs=-1, envvar='OAM_REVIEW_HOSTS')
+def review(day, multicolumn, width, servers):
     """Generate status reports for each server"""
-    Review(day, servers).run()
+    pprint.pprint(Review(day, multicolumn, width, servers).run())
     return 0
