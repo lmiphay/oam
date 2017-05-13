cd0fbf6a37104b60950289bf93c09bd8ceac00e3
Paul Healy <lmiphay@gmail.com>
2017-05-13 19:27:29 +0100

    sort the list of merges made

--- oam/fact/merges.py
+++ oam/fact/merges.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
@@ -14,7 +13,7 @@ from oam.merges import Merges
 def fact(day=last_day()):
     """Return a list of merges made for the day"""
     runfiles = DayLog(day).runfiles('merge.log')
-    return { 'merges': list(Merges().run(runfiles).actual()) }
+    return { 'merges': sorted(list(Merges().run(runfiles).actual())) }
 
 @facts.command()
 def merges():
