1d28d8a8d264b71c925dcd9ed3ad4e37903f0133
Paul Healy <lmiphay@gmail.com>
2017-05-13 22:02:19 +0100

    add yaml output option

--- oam/status.py
+++ oam/status.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 
+import yaml
 import click
 from .cmd import cli
 from oam.daylog import last_day
@@ -10,16 +11,21 @@ from oam.report import Report
 
 class Status(object):
 
-    def __init__(self, day):
+    def __init__(self, day, yaml_format):
         self.day = day
+        self.yaml_format = yaml_format
 
     def run(self):
-        return Report(get_facts(self.day)).render()
+        if self.yaml_format:
+            return yaml.dump(get_facts(self.day), default_flow_style=False)
+        else:
+            return Report(get_facts(self.day)).render()
 
 @cli.command()
 @click.option('--day', default=last_day(), help='day logs to process')
-def status(day):
+@click.option('--yaml-format', is_flag=True, help='return status as yaml')
+def status(day, yaml_format):
     """Generate an on the fly status report for the server"""
-    print(Status(day).run())
+    print(Status(day, yaml_format).run())
     return 0
 
