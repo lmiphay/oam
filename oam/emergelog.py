621eddf0011afd19bb25f54fd45da8431bf001c5
Paul Healy <lmiphay@gmail.com>
2017-05-18 21:59:40 +0100

    start status implementation

--- oam/emergelog.py
+++ oam/emergelog.py
@@ -1,35 +1,43 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 from __future__ import print_function
+
 import sys
 import os
 import subprocess
 import logging
-from datetime import datetime
+import datetime
 import click
 from .cmd import cli
 
-class EmergeLog:
+LOGFILE = '/var/log/emerge.log'
 
-    LOGFILE = '/var/log/emerge.log'
+class EmergeLog(object):
 
-    def __init__(self):
-        self.proc = subprocess.Popen(['tail', '-F', self.LOGFILE], stdout=subprocess.PIPE)
+    def __init__(self, logfile=LOGFILE):
+        self.logfile = logfile
         self.ts_format = os.getenv('OAM_TS', '%Y%m%d:%H:%M:%S')
 
+    def timestamp(self, raw_format):
+        return datetime.datetime.fromtimestamp(int(raw_format)).strftime(self.ts_format)
+
+    def last_line(self):
+        return subprocess.check_output(['tail', '-1', self.logfile])
+
+    def status(self, line):
+        pass
+
     def run(self):
+        self.proc = subprocess.Popen(['tail', '-F', self.logfile], stdout=subprocess.PIPE)
         while 1:
             line = self.proc.stdout.readline().decode('utf8').rstrip()
             field = line.split(':', 1)
             if len(field) == 2:
-                print(datetime.fromtimestamp(int(field[0])).strftime(self.ts_format), field[1])
+                print(self.timestamp(field[0]), field[1])
+            else:
+                print(line)
 
 @cli.command()
 def emergelog():
     """Tail /var/log/emerge.log with readable times"""
     EmergeLog().run()
-
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
-
-    sys.exit(EmergeLog().run())
