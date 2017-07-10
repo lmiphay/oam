3e5131651bf10a67739649a6300c2e25b8ccca79
Paul Healy <lmiphay@gmail.com>
2017-07-10 21:51:37 +0100

    link in log module

--- oam/__init__.py
+++ oam/__init__.py
@@ -24,6 +24,7 @@ from oam.flowrunner import FlowRunner
 from oam.expire import OAMExpire, expire
 from oam.genlop import Genlop, genlop
 from oam.go import gocmd, bg
+from oam.log import log
 from oam.merges import Merges, mergesummary
 from oam.news import News, checknews, countnewnews
 from oam.obsolete import obsolete
