78e1400b17ff3618ba2e656f1ea7b0b78ca9916f
Paul Healy <lmiphay@gmail.com>
2017-07-10 18:55:44 +0100

    add eix to sync steps

--- oam/tasks/sync.py
+++ oam/tasks/sync.py
@@ -2,7 +2,17 @@
 
 from invoke import task
 
-@task(default=True)
+@task(name='eix-update')
+def eix_update(ctx):
+    """Update the local eix db after a sync (otherwise eix is broken)"""
+    ctx.run('eix-update --nocolor')
+
+@task(name='eix-remote')
+def eix_remote(ctx):
+    ctx.run('eix-remote -H update1')
+    # ctx.run('eix-remote add1')
+
+@task(default=True, post=[eix_update, eix_remote])
 def sync(ctx):
     """Sync repos which have their auto-sync setting set to true"""
     ctx.run('emaint --auto sync')
