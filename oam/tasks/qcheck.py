94294dc1ffed757a8cca2d324cd87247ae7801d8
Paul Healy <lmiphay@gmail.com>
2017-07-17 21:39:08 +0100

    log qcheck execution

--- oam/tasks/qcheck.py
+++ oam/tasks/qcheck.py
@@ -6,12 +6,15 @@ import invoke
 from invoke import task
 import oam.path
 from oam.qcheck import QCheck
+import oam.log
 
 @task(default=True)
 def qcheck(ctx):
     """( oam qcheck > $(oam_logfile qcheck) ) 2> >(oam_err "qcheck")
        ctx.run('oam qcheck')
     """
+    oam.log.info('qcheck')
+
     with open(oam.path.log_file('qcheck'), 'w') as f:
         for it in QCheck().its():
             f.write('{}\n'.format(it))
