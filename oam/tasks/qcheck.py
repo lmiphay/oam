e8c878cba221e068d1b7c6ae5fd88afc55620ec1
Paul Healy <lmiphay@gmail.com>
2017-07-19 21:49:28 +0100

    add after-diff to qcheck

--- oam/tasks/qcheck.py
+++ oam/tasks/qcheck.py
@@ -4,9 +4,12 @@ import logging
 
 import invoke
 from invoke import task
+
 import oam.path
 from oam.qcheck import QCheck
 import oam.log
+import oam.daylog
+import oam.fact
 
 @task(default=True)
 def qcheck(ctx):
@@ -18,4 +21,20 @@ def qcheck(ctx):
     with open(oam.path.log_file('qcheck'), 'w') as f:
         for it in QCheck().its():
             f.write('{}\n'.format(it))
+
+    diffs = oam.fact.qcheckdiff.fact()['qcheck_diff']
+
+    if len(diffs)<1:
+        with open(oam.path.log_file('qcheck'), 'a') as f:
+            f.write('---------------------\n')
+            f.write('No Differences\n')
+            f.write('---------------------\n')
+    else:
+        with open(oam.path.log_file('qcheck'), 'a') as f:
+            f.write('---------------------\n')
+            f.write('Differences\n')
+            f.write('---------------------\n')
+            for diff_line in diffs:
+                f.write('{}\n'.format(diff_line))
+
     return 0
