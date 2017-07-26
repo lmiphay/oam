7537849f685e739f457e8c61ccb38f9e220fe8fe
Paul Healy <lmiphay@gmail.com>
2017-07-26 21:14:29 +0100

    load up settings from defaults/file/env

--- oam/settings.py
+++ oam/settings.py
@@ -15,9 +15,29 @@ OAM_REVIEW_HOSTS = os.getenv('OAM_REVIEW_HOSTS', 'localhost')
 OAM_SANDBOXWAIT = os.getenv('OAM_SANDBOXWAIT', 8)
 OAM_TS = os.getenv('OAM_TS', '%Y%m%d:%H:%M:%S')
 
+OAM_CONFIG = os.getenv('OAM_CONFIG', '/etc/oam.yaml')
+
 PORTAGE_CONFIGROOT = os.getenv('PORTAGE_CONFIGROOT', '')
 
-GLOBAL_CONFIG = '/etc/oam.yaml'
+def get_config():
+    config = {}
+
+    # set defaults
+    for var in dir(oam.settings):
+        if var.startswith('OAM_'):
+            config[var[4:].lower()] = getattr(oam.settings, var)
+
+    # overwrite/load settings from config file
+    if os.path.exists(OAM_CONFIG):
+        config = yaml.load(open(OAM_CONFIG))
+
+    # if present in the environment then overwrite
+    for var in dir(oam.settings):
+        if var.startswith('OAM_'):
+            if var in os.environ:
+                config[var[4:].lower()] = os.getenv(var)
+
+    return config
 
 def dump():
     for var in dir(oam.settings):
@@ -28,10 +48,8 @@ def get_setting(name, default_value=None):
     return getattr(sys.modules[__name__], name, default_value)
 
 def get_flow(name):
-    if os.path.exists(GLOBAL_CONFIG):
-        config = yaml.load(open(GLOBAL_CONFIG))
-        if type(config) is dict:
-            if 'flows' in config:
-                if name in config['flows']:
-                    return config['flows'][name]
+    config = get_config()
+    if 'flows' in config:
+        if name in config['flows']:
+            return config['flows'][name]
     return None
