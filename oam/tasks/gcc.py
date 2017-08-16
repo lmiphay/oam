d5d32282754aefd0e1655b4a08fdd305828c4c58
Paul Healy <lmiphay@gmail.com>
2017-08-16 21:27:20 +0100

    add extraopt optional param

--- oam/tasks/gcc.py
+++ oam/tasks/gcc.py
@@ -3,6 +3,6 @@
 from invoke import task
 
 @task
-def cpp11gcc5(ctx):
+def cpp11gcc5(ctx, extraopt=''):
     """see: new item from 2015-10-22: GCC 5 Defaults to the New C++11 ABI"""
-    ctx.run("revdep-rebuild --library 'libstdc++.so.6' -- --exclude gcc")
+    ctx.run("revdep-rebuild {} --library 'libstdc++.so.6' -- --exclude gcc".format(extraopt))
