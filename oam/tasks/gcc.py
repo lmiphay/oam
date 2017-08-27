1a79a1d2890c1512428593933b8bebd249ac6716
Paul Healy <lmiphay@gmail.com>
2017-08-27 18:51:55 +0100

    fix install format string; add more notes

--- oam/tasks/gcc.py
+++ oam/tasks/gcc.py
@@ -8,14 +8,20 @@ from invoke import task
 """
 1. host: generate list of packages (manifest) to build on the host
 2. container: configure to match the host
-    a. /etc/portage/make.conf (CPPFLAGS/LDFLAGS, ACCEPT_LICENSE, use flags...)
+    a. /etc/portage/make.conf (CPPFLAGS/LDFLAGS, ACCEPT_LICENSE, VIDEO_CARDS, use flags...)
     b. /etc/portage/package.use
-    c. /etc/portage/package.provided (e.g. may want to provide kernel sources)
+    c. /etc/portage/package.provided (e.g. may want to provide kernel sources (couldn't get that to work))
     d. /etc/portage/package.keywords for relevant packages
     d. depclean the container (optional)
 3. container: bring up to date (inc newuse) (check /usr/portage/packages is writable from container)
-4. container: parse list and build a binary package of anything which is not already available as a binary package
-5. host: install the binary packages
+4. container: build a binary package of anything which is not already available as a binary package (use manifest from step 1)
+5. host: switch to gcc 5.x and install the binary packages (with extraopts='--pretend' first)
+6  clean binary packages down
+
+Notes:
+   if qt conflicts, just remove from manifest and let portage decide which packages are actually needed
+   drop eix if the eix cache is bind mounted ro into the container (it tries to write to it)
+   maybe drop chromium, libreoffice from manifest and rebuild on host later
 """
 
 PKGDIR = os.getenv('PKGDIR', '/usr/portage/packages')
@@ -68,7 +74,7 @@ def build(ctx, manifest, extraopt=''):
 @task
 def install(ctx, manifest, extraopt=''):
     """host: install packages"""
-    ctx.run('emerge {} {} {}'.format(extraopt, INSTALL_OPT, ' ='.join(packages(manifest))),
+    ctx.run('emerge {} {} ={}'.format(extraopt, INSTALL_OPT, ' ='.join(packages(manifest))),
             echo=True,
             capture_buffer_size=200)
 
