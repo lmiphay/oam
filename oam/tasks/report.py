530a723a5c9885c25807983d7b537e33728be91c
Paul Healy <lmiphay@gmail.com>
2017-09-23 21:30:09 +0100

    working report generation

--- oam/tasks/report.py
+++ oam/tasks/report.py
@@ -2,17 +2,20 @@
 
 from invoke import task
 
-import oam.daylog
+from oam.daylog import last_day
 import oam.facts
-import oam.report
+from oam.report import Report
 import oam.settings
 
 # daydir should be similar to: 20160403 
 @task(default=True)
-def report(ctx, daydir=oam.daylog.last_day())
+def report(ctx, daydir=last_day()):
     logdir = oam.settings.oam.logs.directory  # e.g. /var/log/oam
-
-    with ctx.cd('{}/{}'.format(logdir, daydir)):
+    curdir = os.getcwd()
+    try:
+        os.chdir('{}/{}'.format(logdir, daydir))
         oam.facts.write_facts(daydir, 'summary.yaml')
         with open('summary.log', 'w') as summary:
-            summary.write(oam.report.Report().build(['summary.yaml']).render('summary.jinja2'))
+            summary.write(Report().build(['summary.yaml']).render('summary.jinja2'))
+    finally:
+        os.chdir(curdir)
