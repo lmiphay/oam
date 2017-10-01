408b881b977db793842bcbe0749e9efb79d11ad7
Paul Healy <lmiphay@gmail.com>
2017-10-01 21:27:32 +0100

    start on phased empty tree rebuild support

--- oam/tasks/emptytree.py
+++ oam/tasks/emptytree.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
+import os
+import re
+
 from invoke import task
 
 @task
@@ -16,14 +20,31 @@ def gcc_latest(ctx):
 
 @task(gcc_current, gcc_latest)
 def gcc_current_is_latest(ctx, gcc_version_check=True):
+    """force an error unless the currently selected gcc is not the newest version installed"""
     if gcc_version_check and (ctx.gcc.current != ctx.gcc.latest):
         raise RuntimeError('current gcc ({}) is not the latest installed({})'.format(ctx.gcc.current, ctx.gcc.latest))
 
 @task(gcc_current_is_latest)
 def system(ctx):
+    """rebuild the system packages (twice for luck)"""
     for i in range(2):
         ctx.emerge('@system')
 
 @task(default=True, pre=[gcc_current_is_latest])
-def emptytree(ctx):
-    ctx.emerge('--keep-going --emptytree world', env={'I_KNOW_WHAT_I_AM_DOING': '1'})
+def emptytree(ctx, extraopt=''):
+    """rebuild everything in world and needed by it"""
+    ctx.emerge('--keep-going --emptytree {extraopt} world',
+               extraopt=extraopt,
+               env={'I_KNOW_WHAT_I_AM_DOING': '1'})
+
+def emergelog_current():
+    return os.stat('/var/log/emerge.log').st_size
+
+@task
+def manifest(ctx):
+    """generate a list of packages which would be build for an emptytree rebuild"""
+    print('emergelog_current {}'.format(emergelog_current))
+    emptytree(ctx, '--pretend')
+
+# 1506776832:  ::: completed emerge (43 of 44) sys-fs/e2fsprogs-1.43.3-r1 to /
+MERGE_RECORD = r'(\d+)::  ::: completed emerge \(\d+ of \d+\) ([-\w\d\.]+) to '
