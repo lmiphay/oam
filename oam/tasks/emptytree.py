b0a219b6ad65f7fd84f8b28277430e8d33de603a
Paul Healy <lmiphay@gmail.com>
2017-10-17 21:50:01 +0100

    use the config tasks to check for latest gcc/binutils

--- oam/tasks/emptytree.py
+++ oam/tasks/emptytree.py
@@ -5,46 +5,36 @@ import os
 import re
 
 from invoke import task
+from invoke.tasks import call
 
-@task
-def setup(ctx):
-    ctx.gcc = {}
+from oam.tasks.config import is_latest
+import oam.tasks.resume
 
-@task(setup)
-def gcc_current(ctx):
-    ctx.gcc.current = ctx.run('gcc-config -c', hide='out').stdout.strip()
+EMPTYTREE_ENV = {'I_KNOW_WHAT_I_AM_DOING': '1'}
 
-@task(setup)
-def gcc_latest(ctx):
-    ctx.gcc.latest = ctx.run("gcc-config -l -C | tail -1 | awk '{print $2}'", hide='out').stdout.strip()
 
-@task(gcc_current, gcc_latest)
-def gcc_current_is_latest(ctx, gcc_version_check=True):
-    """force an error unless the currently selected gcc is not the newest version installed"""
-    if gcc_version_check and (ctx.gcc.current != ctx.gcc.latest):
-        raise RuntimeError('current gcc ({}) is not the latest installed({})'.format(ctx.gcc.current, ctx.gcc.latest))
+@task(pre=[call(is_latest, profile='gcc'), call(is_latest, profile='binutils')])
+def check(ctx):
+    print('pre-checks done')
 
-@task(gcc_current_is_latest)
+@task(check)
 def system(ctx):
     """rebuild the system packages (twice for luck)"""
     for i in range(2):
         ctx.emerge('@system')
 
-@task(default=True, pre=[gcc_current_is_latest])
+@task(default=True, pre=[check])
 def emptytree(ctx, extraopt=''):
-    """rebuild everything in world and needed by it"""
-    ctx.emerge('--keep-going --emptytree {extraopt} world',
-               extraopt=extraopt,
-               env={'I_KNOW_WHAT_I_AM_DOING': '1'})
-
-def emergelog_current():
-    return os.stat('/var/log/emerge.log').st_size
+    """reinstall world and its 'entire dependency tree'"""
+    ctx.emerge('--keep-going --emptytree {extraopt} world'.format(extraopt=extraopt),
+               env=EMPTYTREE_ENV)
 
 @task
-def manifest(ctx):
-    """generate a list of packages which would be build for an emptytree rebuild"""
-    print('emergelog_current {}'.format(emergelog_current))
-    emptytree(ctx, '--pretend')
+def resume(ctx, extraopt=''):
+    """resume an interrupted emptytree world merge"""
+    oam.tasks.resume.resume(ctx, extraopt=extraopt,  env=EMPTYTREE_ENV)
 
-# 1506776832:  ::: completed emerge (43 of 44) sys-fs/e2fsprogs-1.43.3-r1 to /
-MERGE_RECORD = r'(\d+)::  ::: completed emerge \(\d+ of \d+\) ([-\w\d\.]+) to '
+@task(pre=[call(emptytree, extraopt='--pretend'))
+def manifest(ctx):
+    """list packages to be built by an emptytree merge"""
+      pass
