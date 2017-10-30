e1456c4e0c4569fbc368fff7852f23284aff9181
Paul Healy <lmiphay@gmail.com>
2017-10-30 16:30:24 +0000

    update for p152; add chown post step

--- oam/tasks/jdk.py
+++ oam/tasks/jdk.py
@@ -14,44 +14,47 @@ jdk:
   site: http://download.oracle.com/otn-pub/java/jdk
   header: '--header "Cookie: oraclelicense=accept-securebackup-cookie"'
   opts: '--continue --no-check-certificate --no-cookies'
-  8u144: b01/090f390dda5b47b9b721c7dfaa008135
+  version: 8u152
+  specific_url: b16/aa0333dd3019491ca4f6ddbe78cdb6d0
 """
 
-# http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
-# e.g.: http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz
-# VER = '8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz'
-# BASE_URL = 'http://download.oracle.com/otn-pub/java/jdk'
-# HEADER = '--header "Cookie: oraclelicense=accept-securebackup-cookie"'
-# OPTS = '--continue --no-check-certificate --no-cookies'
-#
+# http://download.oracle.com/otn-pub/java/jdk/8u144-b01/090f390dda5b47b9b721c7dfaa008135/jdk-8u144-linux-x64.tar.gz
 # http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
+# http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz
+# http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jdk-8u152-linux-x64.tar.gz
 
 # in blocks and merge log files
 MATCH = 'Oracle requires you to download the needed files manually after'
+
+DIRECTORY = '/usr/portage/distfiles'
 
 def jdk_filename():
     return 'jdk-{version}-linux-x64.tar.gz'.format(version=oam.settings.jdk.version)
 
 
 def url():
-    """ http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz """
+    """http://download.oracle.com/otn-pub/java/jdk/8u151-b12/e758a0de34e24606bca991d704f6dcbf/jdk-8u151-linux-x64.tar.gz"""
     return '{site}/{version}-{specific_url}/{filename}'.format(site=oam.settings.jdk.site,
                                                                version=oam.settings.jdk.version,
                                                                specific_url=oam.settings.jdk.specific_url,
                                                                filename=jdk_filename())
 
 @task
-def chown(ctx):
-    ctx.run('chown portage:portage {filename}'.format(filename=jdk_filename()))
+def chown(ctx, directory=DIRECTORY):
+    ctx.run('chown portage:portage {directory}/{filename}'.format(directory=directory,
+                                                                  filename=jdk_filename()),
+            echo=True)
 
 
-@task(default=True. post=[chown])
-def fetch(ctx, directory='/usr/portage/distfiles'):
+@task(default=True, post=[chown])
+def fetch(ctx, directory=DIRECTORY):
     """fetch the oracle jdk to directory"""
     with ctx.cd(directory):
         ctx.run('wget {opts} {header} {url}'.format(opts=oam.settings.jdk.opts,
                                                     header=oam.settings.jdk.header,
-                                                    url=url()))
+                                                    url=url()),
+                echo=True)
+
 @task
 def pretend(ctx):
     print(url())
