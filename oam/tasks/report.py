25a0c0c6c4dda42a984c72cf8e99bc06b1445539
Paul Healy <lmiphay@gmail.com>
2017-12-02 19:40:47 +0000

    don't select current day up front - give todays work a chance to appear

--- oam/tasks/report.py
+++ oam/tasks/report.py
@@ -10,7 +10,9 @@ import oam.settings
 import oam.tasks.config
 
 @task
-def facts(ctx, daydir=last_day()):
+def facts(ctx, daydir=None):
+    if not daydir:
+        daydir = last_day()
     context = oam.facts.get_facts(daydir)
     context.update(oam.tasks.config.profiles(ctx))
     return context
@@ -22,7 +24,9 @@ def write(ctx, context, out_filename='summary.log', template='summary.jinja2'):
 
 # daydir should be similar to: 20160403 
 @task(default=True)
-def report(ctx, daydir=last_day()):
+def report(ctx, daydir=None):
+    if not daydir:
+        daydir = last_day()
     logdir = oam.settings.oam.logs.directory  # e.g. /var/log/oam
     curdir = os.getcwd()
     try:
