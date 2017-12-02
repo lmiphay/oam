e40085824d928abb14aa5e7b0b146cbbc292d61d
Paul Healy <lmiphay@gmail.com>
2017-12-02 20:46:43 +0000

    add minimum package rebuild as default

--- oam/tasks/profile17.py
+++ oam/tasks/profile17.py
@@ -56,8 +56,13 @@ def base(ctx):
 
 @task
 def world(ctx):
-    merge('-e @world')
+    merge('--keep-going -e @world')
 
-@task(default=True, pre=[gcc_config, binutils, libtool, profile, base], post=[world])
+@task
+def minimum(ctx):
+    """see: https://forums.gentoo.org/viewtopic-p-8149548.html#8149548"""
+    merge('/lib*/*.a /usr/lib*/*.a')
+
+@task(default=True, pre=[gcc_config, binutils, libtool, profile, base], post=[minimum])
 def update(ctx):
     pass
