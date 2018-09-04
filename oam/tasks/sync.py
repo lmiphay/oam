68d99fb1e123496b6090832aee8c0dafe3087781
Paul Healy <lmiphay@gmail.com>
2018-09-04 21:45:43 +0100

    add an eix wrapper task; echo commands (for now)

--- oam/tasks/sync.py
+++ oam/tasks/sync.py
@@ -5,14 +5,19 @@ from invoke import task
 @task(name='eix-update')
 def eix_update(ctx):
     """Update the local eix db after a sync (otherwise eix is broken)"""
-    ctx.run('eix-update --nocolor')
+    ctx.run('eix-update --nocolor', echo=True)
 
 @task(name='eix-remote')
 def eix_remote(ctx):
-    ctx.run('eix-remote -H update1')
+    """Fetch the layman eix db's and add to eix db"""
+    ctx.run('eix-remote -H update1', echo=True)  # -H = "Suppress status line update"
     # ctx.run('eix-remote add1')
 
-@task(default=True, post=[eix_update, eix_remote])
+@task(post=[eix_update, eix_remote])
+def eix(ctx):
+    pass
+
+@task(default=True, post=[eix])
 def sync(ctx):
     """Sync repos which have their auto-sync setting set to true"""
-    ctx.run('emaint --auto sync')
+    ctx.run('emaint --auto sync', echo=True)
