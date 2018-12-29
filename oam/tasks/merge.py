e62f92b4c0a98b18d141c0692c9ba261228d47d8
Paul Healy <lmiphay@gmail.com>
2018-12-29 14:15:28 +0000

    add docstring

--- oam/tasks/merge.py
+++ oam/tasks/merge.py
@@ -6,4 +6,5 @@ DEFAULT_OPTS = '--backtrack=50 --deep --verbose --verbose-conflicts'
 
 @task
 def emerge(ctx, default_opts=DEFAULT_OPTS, opts='', target='world', tag=''):
+    """run a niced emerge"""
     ctx.run('ionice -c3 {} /usr/bin/emerge {} {} {}'.format(tag, default_opts, opts, target))
