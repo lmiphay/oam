99aadd46794002f6f5c9635e044a1412a39dc385
Paul Healy <lmiphay@gmail.com>
2018-12-29 14:14:12 +0000

    add support for rebuilding x11

--- oam/tasks/rebuild.py
+++ oam/tasks/rebuild.py
@@ -3,27 +3,48 @@
 import os
 from invoke import task
 
+REBUILD = "emerge -v1 $(equery -q list --format='$cp' {}/*)"
+
+@task
+def x11_libs(ctx):
+    ctx.run(REBUILD.format('x11-libs'))
+
+@task
+def x11_base(ctx):
+    ctx.run(REBUILD.format('x11-base'))
+
+@task
+def x11_drivers(ctx):
+    ctx.run(REBUILD.format('x11-drivers'))
+
+@task(x11_libs,x11_base,x11_drivers)
+def x11(ctx):
+    pass
 
 @task
 def qt(ctx):
-    ctx.run("emerge -v1 $(equery -q list --format='$cp' dev-qt/*)")
+    ctx.run(REBUILD.format('dev-qt'))
 
 @task
 def kde_frameworks(ctx):
-    ctx.run("emerge -v1 $(equery -q list --format='$cp' kde-frameworks/*)")
+    ctx.run(REBUILD.format('kde-frameworks'))
 
 @task
 def plasma(ctx):
-    ctx.run("emerge -v1 $(equery -q list --format='$cp' kde-plasma/*)")
+    ctx.run(REBUILD.format('kde-plasma'))
 
 @task
 def kde_apps(ctx):
-    ctx.run("emerge -v1 $(equery -q list --format='$cp' kde-apps/*)")
+    ctx.run(REBUILD.format('kde-apps'))
 
 @task
 def kde_misc(ctx):
-    ctx.run("emerge -v1 $(equery -q list --format='$cp' kde-misc/*)")
+    ctx.run(REBUILD.format('kde-misc'))
 
 @task(pre=[qt,kde_frameworks,plasma,kde_apps,kde_misc])
 def kde(ctx):
+    pass
+
+@task(x11,kde)
+def all(ctx):
     pass
