10a2bf4ca3dc554ae65b55ab9f7df025a3bc510a
Paul Healy <lmiphay@gmail.com>
2019-10-20 19:33:03 +0100

    comment out eliot support for the moment

--- oam/go.py
+++ oam/go.py
@@ -7,8 +7,8 @@ import subprocess
 import logging
 import time
 import click
-import eliot
-import eliottree
+# import eliot
+# import eliottree
 import codecs
 
 from .cmd import cli
@@ -21,55 +21,55 @@ def gocmd():
     default_flow = oam.settings.oam.go
     return Inv().run_flows([default_flow])
 
-def render_stdout(message):
-    """Ref:
-       http://stackoverflow.com/questions/42936027/how-to-generate-eliot-tasks-for-eliottree-render-tasks
-    """
-    eliottree.render_tasks(codecs.getwriter('utf-8')(sys.stdout).write,
-                           eliottree.tasks_from_iterable([message]),
-                           colorize=True,
-                           human_readable=True)
+# def render_stdout(message):
+#     """Ref:
+#        http://stackoverflow.com/questions/42936027/how-to-generate-eliot-tasks-for-eliottree-render-tasks
+#     """
+#     eliottree.render_tasks(codecs.getwriter('utf-8')(sys.stdout).write,
+#                            eliottree.tasks_from_iterable([message]),
+#                            colorize=True,
+#                            human_readable=True)
 
-FG_CMD = 'ssh {} "{}"'
-BG_CMD = 'ssh {} "screen -dmS {} {}"'
-SESSION_NAME = 'bg-oam'
+# FG_CMD = 'ssh {} "{}"'
+# BG_CMD = 'ssh {} "screen -dmS {} {}"'
+# SESSION_NAME = 'bg-oam'
 
-@cli.command(name='bg')
-@click.option('--wait/--no-wait', default=False, help="wait for operations to complete")
-@click.option('--command', default='oam go', help="command to run")
-@click.option('--raweliot', default=True, help='Output raw eliot logs')
-@click.argument('targets', nargs=-1)
-def bg(wait, command, raweliot, targets):
-    """Run the default oam operation on targets"""
-    if raweliot:
-        eliot.to_file(sys.stdout)
-    else:
-        # eliottree.render_tasks(sys.stdout.write, tasks, colorize=True) #py3
-        eliot.add_destination(render_stdout)
-    procs = []
-    if len(targets)==0:
-        targets = ['localhost']
-    with eliot.start_task(action_type='run_ops', targets=targets):
-        with eliot.start_action(action_type='start_ops', targets=targets):
-            for server in targets:
-                if wait:
-                    cmd = FG_CMD.format(server, command)
-                else:
-                    cmd = BG_CMD.format(server, SESSION_NAME, command)
-                logging.debug('%s start, cmd: %s', server, cmd)
-                with eliot.start_action(action_type='start_process', target=server, cmd=cmd):
-                    procs.append(subprocess.Popen(cmd, shell=True))
-        finished = 0
-        with eliot.start_action(action_type='wait_finishes', targets=targets):
-            while finished != len(procs):
-                for index, server in enumerate(procs):
-                    logging.debug('looping at %s %d', targets[index], finished)
-                    if not server.poll() is None:
-                        eliot.Message.log(message_type='finish', target=targets[index])
-                        finished += 1
-                time.sleep(1)
-        with eliot.start_action(action_type='wait_terminations', targets=targets):
-            for index, server in enumerate(procs):
-                with eliot.start_action(action_type='wait_process', target=targets[index]):
-                    server.wait()
-                    logging.debug('%s finish, returncode=%d', targets[index], server.returncode)
+# @cli.command(name='bg')
+# @click.option('--wait/--no-wait', default=False, help="wait for operations to complete")
+# @click.option('--command', default='oam go', help="command to run")
+# @click.option('--raweliot', default=True, help='Output raw eliot logs')
+# @click.argument('targets', nargs=-1)
+# def bg(wait, command, raweliot, targets):
+#     """Run the default oam operation on targets"""
+#     if raweliot:
+#         eliot.to_file(sys.stdout)
+#     else:
+#         # eliottree.render_tasks(sys.stdout.write, tasks, colorize=True) #py3
+#         eliot.add_destination(render_stdout)
+#     procs = []
+#     if len(targets)==0:
+#         targets = ['localhost']
+#     with eliot.start_task(action_type='run_ops', targets=targets):
+#         with eliot.start_action(action_type='start_ops', targets=targets):
+#             for server in targets:
+#                 if wait:
+#                     cmd = FG_CMD.format(server, command)
+#                 else:
+#                     cmd = BG_CMD.format(server, SESSION_NAME, command)
+#                 logging.debug('%s start, cmd: %s', server, cmd)
+#                 with eliot.start_action(action_type='start_process', target=server, cmd=cmd):
+#                     procs.append(subprocess.Popen(cmd, shell=True))
+#         finished = 0
+#         with eliot.start_action(action_type='wait_finishes', targets=targets):
+#             while finished != len(procs):
+#                 for index, server in enumerate(procs):
+#                     logging.debug('looping at %s %d', targets[index], finished)
+#                     if not server.poll() is None:
+#                         eliot.Message.log(message_type='finish', target=targets[index])
+#                         finished += 1
+#                 time.sleep(1)
+#         with eliot.start_action(action_type='wait_terminations', targets=targets):
+#             for index, server in enumerate(procs):
+#                 with eliot.start_action(action_type='wait_process', target=targets[index]):
+#                     server.wait()
+#                     logging.debug('%s finish, returncode=%d', targets[index], server.returncode)
